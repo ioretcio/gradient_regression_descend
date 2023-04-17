265a6b5d0f91e88c3479aa416de8ba8805329fda
ioretcio <goncharukyou@gmail.com>
2023-04-17 20:37:18 +0300

    Add project files.

---
+++ grad/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace grad
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            richTextBox1 = new RichTextBox();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            groupBox1 = new GroupBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Dock = DockStyle.Top;
+            button1.Location = new Point(0, 46);
+            button1.Name = "button1";
+            button1.Size = new Size(207, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Обрахувати";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Dock = DockStyle.Fill;
+            richTextBox1.Location = new Point(3, 19);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(874, 583);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // button2
+            // 
+            button2.Dock = DockStyle.Top;
+            button2.Location = new Point(0, 23);
+            button2.Name = "button2";
+            button2.Size = new Size(207, 23);
+            button2.TabIndex = 2;
+            button2.Text = "Змоделювати лін залежність";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Dock = DockStyle.Top;
+            textBox1.Location = new Point(0, 0);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 23);
+            textBox1.TabIndex = 0;
+            textBox1.Text = "1 1";
+            // 
+            // panel1
+            // 
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(textBox1);
+            panel1.Dock = DockStyle.Right;
+            panel1.Location = new Point(880, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(207, 605);
+            panel1.TabIndex = 4;
+            // 
+            // panel2
+            // 
+            panel2.Controls.Add(groupBox1);
+            panel2.Dock = DockStyle.Fill;
+            panel2.Location = new Point(0, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(880, 605);
+            panel2.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(richTextBox1);
+            groupBox1.Dock = DockStyle.Fill;
+            groupBox1.Location = new Point(0, 0);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(880, 605);
+            groupBox1.TabIndex = 2;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Результат";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1087, 605);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Form1";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button button1;
+        private RichTextBox richTextBox1;
+        private Button button2;
+        private TextBox textBox1;
+        private Panel panel1;
+        private Panel panel2;
+        private GroupBox groupBox1;
+    }
+}
---
+++ grad/Form1.cs
@@ -0,0 +1,143 @@
+﻿using System.Windows.Forms;
+using System;
+
+namespace grad
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double[] x = { 1, 1 };
+        double[] y = { 1, 1 };
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int N = x.Length;
+            Random rnd = new Random();
+
+            double close_a = rnd.Next(0, 10);
+            double close_b = rnd.Next(0, 10);
+
+
+
+
+
+            double szd_before;
+            double szd_after;
+            double temp1;
+            double temp2;
+            double sum;
+            double navch = 0.0001;
+            int count = 0;
+            richTextBox1.Text = "";
+
+            do
+            {
+                count += 1;
+
+                sum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    sum += Math.Pow((close_a + close_b * x[i] - y[i]), 2);
+                }
+                szd_before = 0.125 * sum;
+
+
+                sum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    sum += close_a + close_b * x[i] - y[i];
+                }
+                temp1 = close_a - navch * 0.25 * sum;
+
+
+                sum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    sum += (close_a + close_b * x[i] - y[i]) * x[i];
+                }
+                temp2 = close_b - navch * 0.25 * sum;
+
+
+                close_a = temp1;
+                close_b = temp2;
+
+
+                sum = 0;
+                for (int i = 0; i < N; i++)
+                {
+                    sum += Math.Pow((close_a + close_b * x[i] - y[i]), 2);
+                }
+                szd_after = 0.125 * sum;
+
+                if (count % 1000 == 0)
+                {
+                    richTextBox1.Text += ($"{count}]    a:{close_a}      b:{close_b}  cзд:{szd_after}\n");
+                }
+
+            } while (szd_after <= szd_before && count < 50000 && Math.Abs(szd_after - szd_before) > navch / 100);
+            richTextBox1.Text += ($"{count}]    a:{close_a}      b:{close_b}  cзд:{szd_after}\n");
+        }
+        double[] NormModelling(double m, double s, int N)
+        {
+            Random R = new Random();
+            double[] mas = new double[0];
+            int i = 0;
+            while (i <= N - 2)
+            {
+                double x = R.NextDouble();
+                double y = R.NextDouble();
+
+                x = -1 + x * 2;
+                y = -1 + y * 2;
+                double se = Math.Pow(x, 2) + Math.Pow(y, 2);
+                if ((se > 0) && (se <= 1))
+                {
+                    double z0 = x * Math.Sqrt(-2 * Math.Log(se) / se);
+                    double z1 = y * Math.Sqrt(-2 * Math.Log(se) / se);
+                    double eps = m + s * z0;
+                    Array.Resize(ref mas, mas.Length + 1);
+                    mas[mas.Length - 1] = eps;
+                    eps = m + s * z1;
+                    Array.Resize(ref mas, mas.Length + 1);
+                    mas[mas.Length - 1] = eps;
+                    i += 2;
+                }
+            }
+            return mas;
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random R = new Random();
+            
+            var splittedtb1 = textBox1.Text.Split();
+            var a_Parab = Convert.ToDouble(splittedtb1[1]);
+            var b_Parab = Convert.ToDouble(splittedtb1[0]);
+            richTextBox1.Text += $"Лінійну регресію з параметрами {a_Parab} та {a_Parab} та нормально розподіленим зашумленням згенеровано.";
+            int N = 1000;
+            double eps = 1;
+            double Xmin = -1;
+            double Xmax = 1;
+            double[] XXXX = new double[N];
+            double[] esheY = new double[N];
+            for (int i = 0; i < N; i++)
+            {
+                double esheodinX = R.NextDouble();
+                esheodinX = Xmin + esheodinX * (Xmax - Xmin);
+                XXXX[i] = esheodinX;
+            }
+            var epsilon = NormModelling(0, eps, N);
+
+            for (int i = 0; i < epsilon.Length; i++)
+                esheY[i] = a_Parab * XXXX[i] + b_Parab + epsilon[i];
+
+            x = new double[XXXX.Length]; XXXX.CopyTo(x, 0);
+            y = new double[esheY.Length]; esheY.CopyTo(y, 0);
+
+        }
+    }
+}
